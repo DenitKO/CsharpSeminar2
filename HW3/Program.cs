70ceb8e324f5207078895d4a8e5d735794ccf1cf
DenitKO <motovdenis@mail.ru>
2022-08-15 01:09:09 +0300

    add programm HW3

---
+++ HW3/Program.cs
@@ -0,0 +1,23 @@
+﻿int day = 0;
+ 
+    while (true)
+    {
+        Console.WriteLine("Введите день недели (от 1 до 7)");
+        day = Convert.ToInt32(Console.ReadLine());
+ 
+        if (day < 1 || day > 7)
+        {
+            continue;
+        }
+ 
+        break;
+    }
+if (day > 5 & day < 8)
+{
+    Console.Write("Сегодня выходной день, и это ");
+    Console.WriteLine(day);
+} else
+{
+    Console.Write("Сегодня будние, и это ");
+    Console.WriteLine(day);    
+}
